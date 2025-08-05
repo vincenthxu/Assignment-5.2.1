f9af248c672ba1ceee2ba5657964b009b5fd0bd8
Vincent Xu <vincent.xu1@outlook.com>
2025-08-05 13:32:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Assignment_5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            if (args.Length > 0)
+            {
+                foreach (var arg in args)
+                {
+                    s += arg + " ";
+                }
+            }
+            else
+            {
+                s = " Hello World ";
+            }
+            Console.WriteLine($"{s}: {LastWordLength(s)}");
+        }
+
+        static int LastWordLength(string sentence)
+        {
+            int count = 0;
+            for (int i = sentence.Length - 1; i >= 0; i--)
+            {
+                if (sentence[i] == ' ')
+                {
+                    if (count == 0)
+                        continue;
+                    break;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
